e09fd0ae68387fd6ab9b012bc0f7f8dcd30e6934
dell <piyush6348@gmail.com>
2017-02-03 01:44:24 +0530

    feat: Receive Reengagement Notifications

--- friendlychat/android-start/app/src/main/java/com/google/firebase/codelab/friendlychat/MyFirebaseMessagingService.java
+++ friendlychat/android-start/app/src/main/java/com/google/firebase/codelab/friendlychat/MyFirebaseMessagingService.java
@@ -20,12 +20,13 @@ import android.util.Log;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
-public class MyFirebaseMessagingService {
+public class MyFirebaseMessagingService extends FirebaseMessagingService{
 
     private static final String TAG = "MyFMService";
 
+    @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-        // Handle data payload of FCM messages.
+        // Handle data payload of FCM messages.O
         Log.d(TAG, "FCM Message Id: " + remoteMessage.getMessageId());
         Log.d(TAG, "FCM Notification Message: " + remoteMessage.getNotification());
         Log.d(TAG, "FCM Data Message: " + remoteMessage.getData());
